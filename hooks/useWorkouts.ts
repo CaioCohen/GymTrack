8e47b4b4f1818e7e4af57270ca3e383d0f2fbce3
Caio Cohen <caiocds1@gmail.com>
2026-02-05 14:45:27 -0300

    Add EAS configuration and update workout hooks
    - Introduced eas.json for EAS build configuration.
    - Updated app.json with additional package information and extra properties.
    - Removed AsyncStorage.clear() call from useWorkouts hook to prevent unintended data loss.
    - Updated icon image file.

--- hooks/useWorkouts.ts
+++ hooks/useWorkouts.ts
@@ -1,6 +1,5 @@
 import seed from '@/assets/seed.json';
 import { loadWorkouts, saveWorkouts } from '@/storage/workoutStorage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 
 export function useWorkouts() {
@@ -8,7 +7,6 @@ export function useWorkouts() {
 
   useEffect(() => {
     (async () => {
-      await AsyncStorage.clear();
       const stored = await loadWorkouts();
       if (stored) setWorkouts(stored);
       else {
